0773139cf76cc2468824273d7df4906424a57186
LucaPanas <49339752+elpanas@users.noreply.github.com>
2020-11-02 14:00:32 +0100

    Add files via upload

---
+++ defaults/preferences/forumeye.js
@@ -0,0 +1,59 @@
+// Controllo data
+pref('extensions.everyeyeNW.data_evento', 0);
+pref('extensions.everyeyeNW.data_news', 0);
+
+// Preferenze reset
+pref('extensions.everyeyeNW.dataReset', 0);
+pref('extensions.everyeyeNW.intervalloReset', 2592000); // 30 giorni
+pref('extensions.everyeyeNW.update_time', 3600); // 1 ora
+
+// News non lette
+pref('extensions.everyeyeNW.newsNR1', '');
+pref('extensions.everyeyeNW.newsNR2', '');
+pref('extensions.everyeyeNW.newsNR3', '');
+
+// News lette
+pref('extensions.everyeyeNW.newsR1', '');
+pref('extensions.everyeyeNW.newsR2', '');
+pref('extensions.everyeyeNW.newsR3', '');
+
+// Totale eventi e news
+pref('extensions.everyeyeNW.evento_tot', 0);
+pref('extensions.everyeyeNW.game_tot', 0);
+pref('extensions.everyeyeNW.contest_tot', 0);
+pref('extensions.everyeyeNW.torn_tot', 0);
+
+// Generale
+pref('extensions.everyeyeNW.videogiochi', true);
+pref('extensions.everyeyeNW.movieye', true);
+pref('extensions.everyeyeNW.serialeye', true);
+pref('extensions.everyeyeNW.animeye', true);
+pref('extensions.everyeyeNW.techeye', true);
+pref('extensions.everyeyeNW.altro', true);
+pref('extensions.everyeyeNW.eyech', true);
+pref('extensions.everyeyeNW.webtv', true);
+pref('extensions.everyeyeNW.forumeye', true);
+pref('extensions.everyeyeNW.evento_attuale', true); 
+
+// Videogiochi
+pref('extensions.everyeyeNW.x360', true);
+pref('extensions.everyeyeNW.xone', true);
+pref('extensions.everyeyeNW.pc', true);
+pref('extensions.everyeyeNW.ps3', true);
+pref('extensions.everyeyeNW.ps4', true);
+pref('extensions.everyeyeNW.psvita', true);
+pref('extensions.everyeyeNW.wiiu', true);
+pref('extensions.everyeyeNW.3ds', true);
+pref('extensions.everyeyeNW.mobile', true);
+
+// Forumeye Generale
+pref('extensions.everyeyeNW.forum-nextgen', true);
+pref('extensions.everyeyeNW.forum-general', true);
+pref('extensions.everyeyeNW.forum-digital', true);
+pref('extensions.everyeyeNW.forum-rubriche', true);
+pref('extensions.everyeyeNW.forum-magazine', true);
+pref('extensions.everyeyeNW.forum-tornei', true);
+pref('extensions.everyeyeNW.forum-market', true);
+pref('extensions.everyeyeNW.forum-com', true);
+pref('extensions.everyeyeNW.forum-staff', true);
+pref('extensions.everyeyeNW.forum-eventi', true);
